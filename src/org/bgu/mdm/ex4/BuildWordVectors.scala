31cea0ede0044954a60d3615ba8589c6ec6e54ea
Lieberman, Yuval <Yuval.Lieberman@2bprecisehealth.com>
2017-02-23 19:10:55 +0200

    some progress...

--- src/org/bgu/mdm/ex4/BuildWordVectors.scala
+++ src/org/bgu/mdm/ex4/BuildWordVectors.scala
@@ -6,10 +6,13 @@ import org.apache.spark.sql._
 import org.apache.spark.streaming._
 import scala.collection._
 import java.io._
+import scala.util.Random
 import org.apache.spark.rdd.RDD
+import org.apache.spark.mllib.linalg.{Vector , Vectors}
 import org.apache.spark.mllib.linalg.distributed.{ CoordinateMatrix, MatrixEntry }
 import org.apache.spark.mllib.rdd.RDDFunctions._
 import org.apache.spark.mllib.optimization._
+//import breeze.linalg._
 
 object BuildWordVectors extends App {
 
@@ -20,7 +23,7 @@ object BuildWordVectors extends App {
   val VECTOR_SIZE = 50 // גודל הווקטור לייצוג המילה
   val MAX_ITER = 15 // מספר איטרציות
   val X_MAX = 100 //  ערכו של X_MAX בנוסחה 9 במאמר
-  val Alpha = 0.75 // ערכו של פרמטר Alpha בנוסחה 9 במאמר
+  val ALPHA = 0.75 // ערכו של פרמטר Alpha בנוסחה 9 במאמר
 
   var startTime = System.nanoTime();
 
@@ -50,6 +53,34 @@ object BuildWordVectors extends App {
   // restore saved RDD's
   val wordKeys = sc.objectFile[(String, Long)](savedRDDfiles + "Keys")
   val counts = sc.objectFile[((Long, Long), Int)](savedRDDfiles)
+  
+  val vocabularySize = wordKeys.count();
+  
+  // build w - RDD with key = word key, value = word vector  
+  var w = wordKeys.map( word => (word._2 , Vectors.dense(Array.fill(VECTOR_SIZE)(Random.nextDouble - 0.5)) ) ).collectAsMap()
+  val W = sc.broadcast(w)
+  
+  // build b - RDD with key = word key, value = word bias
+  var b = wordKeys.map( word => (word._2 , Random.nextDouble - 0.5) ).collectAsMap()
+  val B = sc.broadcast(b)
+      
+  // weighting function - equation 9
+  def weighting(x: Int) : Double = {
+    return if (x < X_MAX) 
+      Math.pow(x / X_MAX, ALPHA)
+    else
+      1
+   }
+  
+
+  val cost = counts.map( pair => ( 
+      weighting(pair._2) * 
+      Math.pow( W.value(pair._1._1)  *  W.value(pair._1._2)  +
+          B.value(pair._1._1) + B.value(pair._1._1) +
+          - Math.log(pair._2)          
+          , 2) ) )
+          .reduce( _ + _ )
+        
   // convert counts to matrix format
   val wordsMatrix = new CoordinateMatrix(counts.map(item => new MatrixEntry(item._1._1, item._1._2, item._2)));
 
@@ -57,7 +88,7 @@ object BuildWordVectors extends App {
   // -----------------------------
   val updater = new SimpleUpdater() // do we need L1 / L2 updater?
   val gradient = new LeastSquaresGradient() // do we need a custom gradient?
-  GradientDescent.runMiniBatchSGD(data, gradient, updater, stepSize, MAX_ITER, regParam, miniBatchFraction, initialWeights)
+//  GradientDescent.runMiniBatchSGD(data, gradient, updater, stepSize, MAX_ITER, regParam, miniBatchFraction, initialWeights)
   
   
   log("End word vectors, saving to " + savedVectorFiles);
